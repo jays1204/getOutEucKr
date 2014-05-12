b83e563d5b85be6923302bbbaa47341fc38d3c7c
jays <jays.woo@sk.com>
2014-05-12 10:50:33 +0900

    fixed logic and structure

--- getOutEucKr.sh
+++ getOutEucKr.sh
@@ -1,84 +1,63 @@
 #!/usr/bin/env bash
 
+#argument declaration
 repositoryPath=$1
-fileType=$2
+excludeFileType=$2
 
-echo $repositoryPath
-# Check Validation
-if [ "$repositoryPath" == "" ];
-then
-  echo "Input correct path"
-  exit -1
-fi
+#validation function
+function checkEssentiallibrary {
+  existIconv=$(which iconv | wc -l)
 
-# split extension
-for ex in ${fileType/,/ } ;
-do
-  echo ex
-done
-
-# get List of repository files
-javaList=`find $repositoryPath -name \*.java -printf "%h/%f\n"`
-xmlList=`find $repositoryPath -name \*.xml -printf "%h/%f\n"`
-sqlList=`find $repositoryPath -name \*.sql -printf "%h/%f\n"`
-propertiesList=`find $repositoryPath -name \*.properties -printf "%h/%f\n"`
-
-if [ "$javaList" == "" ];
-then
-  echo "No *.java file"
-else
-  for i in $javaList:
-  do
-    filePath=${i/.java\:/.java}
-    ISEUC=checkEuc filePath
-    iconv -c -f euc-kr -t utf-8 $filePath > $filePath.tmp && mv $filePath.tmp $filePath
-    echo $filePath
-  done
-fi
-
-if [ "$xmlList" == "" ];
-then
-  echo "*.xml file not exist"
-else
-  for i in $xmlList:
-  do
-    filePath=${i/.xml\:/.xml}
-    kccResult=`kcc -c $filePath`
-    
-    iconv -c -f euc-kr -t utf-8 $filePath > $filePath.tmp && mv $filePath.tmp $filePath
-    echo $filePath
-  done
-fi
-
-if [ "$sqlList" == "" ];
-then
-  echo "*.sql file not exist"
-else
-  for i in $sqlList:
-  do
-    filePath=${i/.sql\:/.sql}
-    iconv -c -f euc-kr -t utf-8 $filePath > $filePath.tmp && mv $filePath.tmp $filePath
-    echo $filePath
-  done
-fi
-
-if [ "$propertiesList" == "" ];
-then
-  echo "*.properties file not exist"
-else
-  for i in $propertiesList:
-  do
-    filePath=${i/.properties\:/.properties}
-    iconv -c -f euc-kr -t utf-8 $filePath > $filePath.tmp && mv $filePath.tmp $filePath
-    echo $filePath
-  done
-fi
-
-function checkEuc() {
-  if [[ "$1" == *EUC* ]];
-  then 
-    return true
-  else
-    return false
+  if [ "$existIconv" == "" ];
+  then
+    echo "Please Install iconv!"
+    exit -1
   fi
 }
+
+function checkValidDirectory {
+  if [ "$repositoryPath" == "" ];
+  then
+    echo "Input correct path"
+    exit -1
+  fi
+
+  if [ -d $repositoryPath ];
+  then
+    
+  else
+    echo "$repositoryPath is not directory or not exist."
+    exit -1
+  fi
+}
+
+function isFileInDirectory {
+  fileCount=$(ls | wc -l)
+  if [ $fileCount =< 1 ];
+  then
+    echo "No file In $repositoryPath"
+    exit -1
+  fi
+}
+
+function validArgument {
+  checkEssentiallibrary()
+  checkValidDirectory()
+  isFileInDirectory()
+}
+
+function translateCharacterEncoding {
+  for filePath in "$repositoryPath"/*
+  do
+    iconv -c -f euc-kr -t utf-8 $filePath > $filePath.tmp
+    mv $filePath.tmp $filePath
+  done
+}
+
+#check validation of arguments
+validArgument()
+
+# announce target direcotry 
+echo "$repositoryPath directory is targeted on change character encoding"
+
+translateCharacterEncoding()
